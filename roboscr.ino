94ad808071cb7af23b28f8dfbbc0134b7ae5266f
hforesti <henrique@robolivre.org>
2019-07-23 22:17:39 -0300

    acelera e desacelera no mesmo movimento

--- roboscr.ino
+++ roboscr.ino
@@ -1,27 +1,37 @@
 #define CLK 9 //CLOCK 
 #define ENB 10 //ENABLE 
 #define DIR 8 //DIRECTION 
-
+#define TIMEVELMAX 20    //Tempo em Microsegundos para clock em velocidade máxima
+#define TIMEVELMIN 150  //Tempo em Microsegundos para clock em velocidade mínima
+ 
 bool clkNvl = 0; //Nível do sinal de Clock 
 long timeStamp = micros();
 long timeClk = 100;
 int stepToDo = 0;
+int motorPosition = 0;
+int stepNumber = 0;
 char velMode = 'N'; //N normal, A acelera, D desacelera
-
 void runMotor(void){
   if ((stepToDo) || (velMode == 'C')) {
     if ((micros() - timeStamp) >= timeClk){
+      if ((stepNumber - stepToDo) < (stepNumber/3)){ //MODO ACELERA
+        timeClk = map(stepToDo,(2*stepNumber/3),stepNumber,TIMEVELMAX,TIMEVELMIN);
+      }else{
+        if ((stepNumber - stepToDo) < (2*stepNumber/3)){ // MODO NORMAL = VELOCIDADE MÁXIMA
+          timeClk = TIMEVELMAX;
+        }else{
+          timeClk = (TIMEVELMAX+TIMEVELMIN) - map(stepToDo,1,stepNumber/3,TIMEVELMAX,TIMEVELMIN);//MODO DESACELERA
+        }
+      }
+      //Serial.print("SteptoDo=>");
+      //Serial.print(stepToDo);
+      //Serial.print("timeClk=>");
+      //Serial.println(timeClk);
       timeStamp = micros();
       clkNvl = !clkNvl;
       digitalWrite(CLK,clkNvl);
       if (!clkNvl){
         stepToDo --;
-      }
-      if (velMode == 'A'){
-        timeClk = map(stepToDo,1,10000,50,500);
-      }
-      if (velMode == 'D'){
-        timeClk = 550 - map(stepToDo,1,10000,50,500);
       }
     }
   }else{
@@ -48,8 +58,7 @@ void loop() {
     serialIn = Serial.readString();
     if (serialIn.substring(0,3) == "C2N"){
       stepToDo = 10000;
-      digitalWrite(ENB,LOW);
-      velMode = 'N';
+      stepNumber = 10000;
     }  
     if (serialIn.substring(0,3) == "C2A"){
       stepToDo = 10000;
